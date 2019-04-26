9f2515b550f1e420d9b3d8713ad8c6391a011ac2
Jonathan Hertz <jonathan_hertz2@live.dk>
2019-04-26 20:14:47 +0200

    Dummy scripts

--- Assets/Scripts/HorseController.cs
+++ Assets/Scripts/HorseController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class HorseController : MonoBehaviour
 {
+    //private float 
+
+    [SerializeField] private KeyCode jumpButton;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(jumpButton))
+        {
+            rb.AddForce(Vector2.up * )
+        }
     }
 }
