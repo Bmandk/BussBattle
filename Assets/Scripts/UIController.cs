1d2303493e03295158aa6a3c5930ba7131eb4480
Oscar Rode <oscarrode99@gmail.com>
2019-04-27 23:02:06 +0200

    Setup for sprites of health system

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    // All the sprites for the hearts based on your health
+    public Sprite Heart_1_1;
+    public Sprite Heart_1_2;
+    public Sprite Heart_2_1;
+    public Sprite Heart_2_2;
+    public Sprite Heart_3_1;
+    public Sprite Heart_3_2;
+    public Sprite Heart_4_1;
+    public Sprite Heart_4_2;
+    public Sprite Heart_5_1;
+    public Sprite Heart_5_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
