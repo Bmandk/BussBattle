f0f4b004726b471de58bad8897614a179ae62374
Oscar Rode <oscarrode99@gmail.com>
2019-04-27 10:24:12 +0200

    Spawning system

---
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+    Vector3 MoveSpeed = new Vector3(2f, 0f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.position += MoveSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    public GameObject Obstacle;
+    public float SpawnRate = -1;
+    public Vector3 CurrentPosition = new Vector3(0f, 0f, 0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (SpawnRate < 0)
+        {
+            GameObject ObjectObstacle = GameObject.Instantiate(Obstacle, CurrentPosition, Quaternion.identity) as GameObject;
+            CurrentPosition += new Vector3(1f, 0f, 0f);
+            SpawnRate = 2;
+        }
+
+        SpawnRate = SpawnRate - Time.deltaTime;
     }
 }
