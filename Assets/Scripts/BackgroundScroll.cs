70389f64ddeb9a929aac23cd3aa53603afe408b1
Jonathan Hertz <jonathan_hertz2@live.dk>
2019-04-27 21:35:23 +0200

    Added Background scrolling

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private float _currentScroll;
+
+    private Material _material;
+
+    void Awake()
+    {
+        _material = GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        _currentScroll += _speed * Time.deltaTime;
+        _material.mainTextureOffset = new Vector2(_currentScroll, 0);
+    }
+}
